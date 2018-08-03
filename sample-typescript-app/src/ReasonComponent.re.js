06f35da62eb765870a4439d5b91e2c7c46b22522
Cristiano Calcagno <ccrisccris@gmail.com>
2018-08-03 20:01:33 +0200

    latest genFlow

--- sample-typescript-app/src/ReasonComponent.re.js
+++ sample-typescript-app/src/ReasonComponent.re.js
@@ -1,7 +1,7 @@
 /* @flow strict */
 
-var ReasonComponent = require("./ReasonComponent.bs");
-var ReasonReact = require("reason-react/src/ReasonReact.js");
+const ReasonComponent = require("./ReasonComponent.bs");
+const ReasonReact = require("reason-react/src/ReasonReact.js");
 
 import type {Component as ReactComponent} from 'React';
 import type {actionless as ReasonReactactionless} from './ReasonReact.re';
