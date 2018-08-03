b2298a3b52f2dd02a5f91e2e34f958fd8a9ea982
Cristiano Calcagno <ccrisccris@gmail.com>
2018-08-03 16:25:48 +0200

    Reorder imports.

--- sample-typescript-app/src/index.tsx
+++ sample-typescript-app/src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import {ReasonComponent} from "./ReasonComponent";
 import "./index.css";
+import {ReasonComponent} from "./ReasonComponent";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
