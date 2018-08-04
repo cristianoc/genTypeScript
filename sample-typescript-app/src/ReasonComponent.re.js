c5671eab377e1c50fc82aa7c10b1afba339a06bb
Cristiano Calcagno <ccrisccris@gmail.com>
2018-08-04 10:25:22 +0200

    Master genFLow removes the lasst unnecessary type import.

--- sample-typescript-app/src/ReasonComponent.re.js
+++ sample-typescript-app/src/ReasonComponent.re.js
@@ -5,7 +5,6 @@ const ReasonComponentBS = require("./ReasonComponent.bs");
 // tslint:disable-next-line:no-var-requires
 const ReasonReact = require("reason-react/src/ReasonReact.js");
 
-import type {Component as ReactComponent} from 'React';
 // tslint:disable-next-line:interface-name
 export interface Props {message?:string; children?:any};
 export const component: React.ComponentClass<Props> = ReasonReact.wrapReasonForJs(
