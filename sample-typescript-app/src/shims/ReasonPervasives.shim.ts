cfdaf8ac834a40e48d11b09e689bd4b0f00791ef
Cristiano Calcagno <ccrisccris@gmail.com>
2018-08-06 07:41:55 +0200

    reformat shim

--- sample-typescript-app/src/shims/ReasonPervasives.shim.ts
+++ sample-typescript-app/src/shims/ReasonPervasives.shim.ts
@@ -1,22 +1,22 @@
-
 // tslint:disable-next-line:no-var-requires
-const $$Array = require('bs-platform/lib/js/array');
+const $$Array = require("bs-platform/lib/js/array");
 
-// tslint:disable-next-line:max-classes-per-file 
-export abstract class EmptyList { protected opaque:any };
-
-// tslint:disable-next-line:max-classes-per-file 
-export abstract class Cons<t> { protected opaque:any };
-
-export type list<t> = | Cons<t> | EmptyList;
-
-export function cons(itm:any, lst:any) {
-  return /* :: */[
-          itm,
-          lst
-        ];
+// tslint:disable-next-line:max-classes-per-file
+export abstract class EmptyList {
+  protected opaque: any;
 }
 
-export const emptyList = /* [] */0;
+// tslint:disable-next-line:max-classes-per-file
+export abstract class Cons<t> {
+  protected opaque: any;
+}
+
+export type list<t> = Cons<t> | EmptyList;
+
+export function cons(itm: any, lst: any) {
+  return /* :: */ [itm, lst];
+}
+
+export const emptyList = /* [] */ 0;
 
 export const fromArray = $$Array.to_list;
