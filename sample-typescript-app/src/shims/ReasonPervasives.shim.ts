bc38d7efeb06d456f33a35a55c8c954ee5102714
Cristiano Calcagno <ccrisccris@gmail.com>
2018-08-07 04:18:59 +0200

    Pervasive shims.

--- sample-typescript-app/src/shims/ReasonPervasives.shim.ts
+++ sample-typescript-app/src/shims/ReasonPervasives.shim.ts
@@ -7,16 +7,16 @@ export abstract class EmptyList {
 }
 
 // tslint:disable-next-line:max-classes-per-file
-export abstract class Cons<t> {
-  protected opaque: t;
+export abstract class Cons<T> {
+  protected opaque: T;
 }
 
-export type list<t> = Cons<t> | EmptyList;
+export type list<T> = Cons<T> | EmptyList;
 
-export function cons(itm: any, lst: any) {
-  return /* :: */ [itm, lst];
+export function cons<T>(itm: T, lst: list<T>) : list<T> {
+  return /* :: */ [itm, lst] as any;
 }
 
-export const emptyList = /* [] */ 0;
+export const emptyList : EmptyList = /* [] */ 0 as any;
 
 export const fromArray = $$Array.to_list;
