d6854b875e276c05c9827039f1c464d2fc68fc13
Cristiano Calcagno <ccrisccris@gmail.com>
2018-08-08 11:57:57 +0200

    Add an example of exporting types with type variables.
    Based on current genFlow master.

--- sample-typescript-app/src/ReasonComponent.tsx
+++ sample-typescript-app/src/ReasonComponent.tsx
@@ -22,13 +22,13 @@ export const ReasonComponent: React.ComponentClass<Props> = ReasonReact.wrapReas
 export const minus: (_1:{first?:number; second:number}) => number = function _(Arg1) { const result = ReasonComponentBS.minus(Arg1.first, Arg1.second); return result };
 export const useTypeDefinedInAnotherModule: (_1:Typest) => Typest = ReasonComponentBS.useTypeDefinedInAnotherModule;
 // tslint:disable-next-line:max-classes-per-file 
-export abstract class TA { protected opaque:any }; /* simulate opaque types */
+export abstract class TA { protected opaque: any }; /* simulate opaque types */
 export const A: TA = 0 as any;
 // tslint:disable-next-line:max-classes-per-file 
-export abstract class TB { protected opaque:any }; /* simulate opaque types */
+export abstract class TB { protected opaque: any }; /* simulate opaque types */
 export const B: (_1:number) => TB = function _(Arg1) { return CreateBucklescriptBlock.__(0 as any, [Arg1]) }
 // tslint:disable-next-line:max-classes-per-file 
-export abstract class TC { protected opaque:any }; /* simulate opaque types */
+export abstract class TC { protected opaque: any }; /* simulate opaque types */
 export const C: (_1:string) => TC = function _(Arg1) { return CreateBucklescriptBlock.__(1 as any, [Arg1]) }
 export type t =
   | TA
--- sample-typescript-app/src/nested/Types.re
+++ sample-typescript-app/src/nested/Types.re
@@ -2,7 +2,12 @@
 type t = int;
 
 [@genFlow]
-let someIntList = [1,2,3];
+let someIntList = [1, 2, 3];
 
 [@genFlow]
 let map = List.map;
+
+[@genFlow]
+type typeWithVars('x, 'y, 'z) =
+  | A('x, 'y)
+  | B('z);
